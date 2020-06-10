40ec419f5952a9df2c2a82c73c4aadea135e576a
Nhu Mong Tran <nhumongtran1106@gmail.com>
2020-06-10 18:22:38 +0200

    few changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     siteUrl: `http://htw-m1.surge.sh/`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to our journey on making an awesome game: DADOS`,
+      heading: `Welcome to our journey on making an awesome game: DADOS test CI`,
       maxWidth: 800,
     },
     social: [
